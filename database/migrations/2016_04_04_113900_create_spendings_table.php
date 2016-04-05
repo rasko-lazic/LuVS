476b3583f14d4d1aeef9d3067e676bb724b63473
Rasko Lazic <raskolazic@gmail.com>
2016-04-05 14:13:42 +0200

    basic migrations created

---
+++ database/migrations/2016_04_04_113900_create_spendings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spendings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('amount');
+            $table->longText('description');
+            $table->integer('balance');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('spendings');
+    }
+}
